44e4d685824befea2d5afd99a329b7614afb8b95
Yash Bhayani <yashbhayani3007@gmail.com>
2023-07-06 15:59:25 +0530

    Card Widget

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:cardwidget/ui/uimain.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Card(
+            shadowColor: Colors.green,
+            elevation: 11,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Hello World !', style: mSt1()),
+            ),
+          ),
+        ));
+  }
+}
---
+++ lib/ui/uimain.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+TextStyle mSt1() {
+  return const TextStyle(fontSize: 25);
+}
