823b4719ee943501fd0686916307510bc861d02f
Yash Bhayani <yashbhayani3007@gmail.com>
2023-07-08 23:54:54 +0530

    SwitchingScreen

---
+++ android/app/src/main/kotlin/com/example/switchingscreen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.switchingscreen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/intropage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:switchingscreen/main.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Intro Page',
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome',
+              style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 11),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const MyHomePage(title: "Home Page");
+                  }));
+                },
+                child: const Text(
+                  'Next',
+                  style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+                )
+                /*const Icon(
+                  Icons.skip_next,
+                  size: 100,
+                )*/
+                )
+          ],
+        ),
+      ),
+    );
+  }
+}
